c6435686a92ce28b209bf637de7bfd248ce58475
Yuki <yuki08281993@gmail.com>
2018-12-02 08:07:14 +0900

    compile option

--- hoge.rb
+++ hoge.rb
@@ -1,4 +1,4 @@
-
+require 'optparse'
 
 def apply_rule(word, rule)
     if rule[1].is_a? Proc
@@ -26,6 +26,34 @@ def japanese
     p_weakning + vowel_fusion + palatalization
 end
 
+def latin
+    short_v = /[aiueo](?![aiueo])/
+    long_v = /aa|ii|uu|ee|oo/
+    diphthong = /ai|au|ei|oi|ou/
+    consonant = /[^aeiou]/
+    vowel = /#{short_v}|#{long_v}|#{diphthong}/
+    initialSyllable = /\b(#{consonant})*#{vowel}(#{consonant})*/
+    finalSyllable = /#{vowel}(#{consonant})*\b/
+    medialSyllable = /\B#{vowel}#{consonant}*/
+
+    short_vowel_reduction = [
+        [/#{short_v}l/, 'ul'],
+        [/#{short_v}r/, 'er']
+    ]
+
+    consonant = [
+        [initialSyllable, ->(v) { p "init: #{v}"; v }],
+        [medialSyllable,  ->(v) { p "med: #{v}"; v }],
+        [finalSyllable, ->(v) { p "fin: #{v}"; v }]
+    ]
+
+#    p_weakning = [
+#        [/p/, 'f'], [/\bf/, 'h'], [/f#{v}/, ->(w) { w.tr('f', 'w') }],
+#        [/w#{not_large_v}/, ->(w) { w.tr('w', '') }]
+#    ]
+    consonant
+end
+
 def change(rules, word)
     rules.each { |rule| apply_rule(word, rule) }
     word
@@ -35,12 +63,32 @@ def repl
     loop do
         print '>'
         input = STDIN.gets.strip
-        puts change(japanese, input) unless input.empty?
+        puts change(latin, input) unless input.empty?
     end
 end
 
-sample = 'paru sugite natu kinikerasi sirotapeno koromo posutepu amano kaguyama'
+def sample
+    'lingua latina '
+end
 
-puts sample
-puts change(japanese, sample)
-repl
+def parse
+    opt = OptionParser.new
+    options = { Interactive: false, Engine: 'Japanese' }
+    opt.on('-i', '--Interactive') { |v| options[:Interactive] = v }
+    opt.on('-f VAL', '--File VAL') { |v| options[:File] = v }
+    opt.on('-e VAL', '--Engine VAL') { |v| options[:Engine] = v }
+    opt.parse ARGV
+    options
+end
+
+def main
+    options = parse
+    if options[:Interactive]
+        repl
+    else
+        text = options[:File] ? (File.read options[:File]) : sample
+        puts change(latin, text)
+    end
+end
+
+main
